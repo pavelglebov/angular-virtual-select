4d313aaaa3501ace987e5849d056c38e1cf83e30
dzimarin <dzimarin@elilink.com>
2017-09-07 15:52:03 +0300

    added ui-select for comparison, added repeat source mock

--- docs/app/app.js
+++ docs/app/app.js
@@ -2,16 +2,17 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-    'angular-virtual-select'
+    'angular-virtual-select',
+    'ui.select'
 ])
 .controller('MainController', ['$scope', function($scope) {
-    $scope.test = "TEST";
-    // $scope.selectedItem = null;
-    // $scope.items = [{
-    //     id: 1,
-    //     name: "Igar"
-    // },{
-    //     id: 2,
-    //     name: "Vitya"
-    // }];
+    $scope.selectedItem = null;
+    $scope.selectedItem2 = null;
+    $scope.items = [{
+        id: 1,
+        name: "Igar"
+    },{
+        id: 2,
+        name: "Vitya"
+    }];
 }]);
